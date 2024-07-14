c619121e826d23ba6c3afb915e123059d1ec9071
Siyou-Wu <siyou.wu@wustl.edu>
2024-07-14 12:40:44 -0500

    Update MyJsonViewer.js

--- src/MyJsonViewer.js
+++ src/MyJsonViewer.js
@@ -1,6 +1,122 @@
 import React, { useState } from 'react';
 
 function MyJsonViewer({ json }) {
+    const data3 = {
+        expand: true,
+        children: {
+            t1: {
+                expand: true,
+                children: {
+                    t1_1: {
+                        expand: true,
+                        children: {
+                            t1_1_1: {
+                                expand: true,
+                                children: {
+                                    t1_1_1_1: {
+                                        expand: true,
+                                        children: {
+                                            t1_1_1_1_1: {
+                                                expand: true
+                                            },
+                                            t1_1_1_1_2: {
+                                                expand: true
+                                            }
+                                        }
+                                    },
+                                    t1_1_1_2: {
+                                        expand: true
+                                    }
+                                }
+                            },
+                            t1_1_2: {
+                                expand: true,
+                                children: {
+                                    t1_1_2_1: {
+                                        expand: true
+                                    }
+                                }
+                            }
+                        }
+                    },
+                    t1_2: {
+                        expand: true,
+                        children: {
+                            t1_2_1: {
+                                expand: true,
+                                children: {
+                                    t1_2_1_1: {
+                                        expand: true,
+                                        children: {
+                                            t1_2_1_1_1: {
+                                                expand: true
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            },
+            t2: {
+                expand: true,
+                children: {
+                    t2_1: {
+                        expand: true,
+                        children: {
+                            t2_1_1: {
+                                expand: true,
+                                children: {
+                                    t2_1_1_1: {
+                                        expand: true
+                                    },
+                                    t2_1_1_2: {
+                                        expand: true,
+                                        children: {
+                                            t2_1_1_2_1: {
+                                                expand: true
+                                            }
+                                        }
+                                    }
+                                }
+                            },
+                            t2_1_2: {
+                                expand: true
+                            }
+                        }
+                    },
+                    t2_2: {
+                        expand: true,
+                        children: {
+                            t2_2_1: {
+                                expand: true,
+                                children: {
+                                    t2_2_1_1: {
+                                        expand: true
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            },
+            t3: {
+                expand: true,
+                children: {
+                    t3_1: {
+                        expand: true,
+                        children: {
+                            t3_1_1: {
+                                expand: true
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    };
+    
     const data2 = {
         expand: true,
         children: {
@@ -39,20 +155,23 @@ function MyJsonViewer({ json }) {
         }
     }
 
-    const RenderJson2 = ({ data }) => {
-        //const [isExpanded, setIsExpanded] = useState(data.expand)
+    const RenderJson2 = ({ data, layer }) => {
+        
+        const [isExpanded, setIsExpanded] = useState(data.expand)
         const toggleExpand = () => {
-            console.log("click!")
+            setIsExpanded(!isExpanded);
+            console.log("current data, layer",layer, data)
         }
         return (
             <div>
-                {data.expand && data.children && (
+                <p>layer is {layer}</p>
+                <button onClick={()=> toggleExpand()}>{isExpanded ? '-' : '+'}</button>
+                {isExpanded && data.children && (
                     Object.keys(data.children).map((child) => {
                         return (
                             <div style={{ paddingLeft: "20px" }}> 
                                 {child}
-                                <button onClick={()=> toggleExpand()}>{data.children[child].expand ? '-' : '+'}</button>
-                                {data.children[child] && (<RenderJson2 data={data.children[child]}/>)}
+                                {data.children[child] && (<RenderJson2 data={data.children[child]} layer={layer + 1}/>)}
                             </div>
                         )
                     })
@@ -64,7 +183,7 @@ function MyJsonViewer({ json }) {
 
 
     return (
-        RenderJson2({ data: data2 })
+        RenderJson2({ data: data3, layer: 0 })
     );
 }
 
